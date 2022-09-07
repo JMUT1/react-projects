c705c47840613e9ee01bb23f32915c64c7d08730
Chema Torres <chemaaa94@gmail.com>
2022-09-06 20:38:01 -0500

    birtday today using useState and dummy data

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setpeople] = useState(data)
+
+
+  return <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
+      <button onClick={()=> setpeople([])}>Clear all</button>
+    </section>
+  </main>
 }
 
 export default App;
